b73002228a619ba1410e048581024c75aed97569
MaxSvargal <maxsvargal@gmail.com>
2025-06-18 18:36:01 +0700

    Move to docs dir

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   transpilePackages: ['reactflow', '@reactflow/core', '@reactflow/controls', '@reactflow/background', '@reactflow/minimap'],
   output: 'export',
+  distDir: 'docs'
 }
 
 module.exports = nextConfig 

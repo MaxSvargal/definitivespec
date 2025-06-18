328395dfe8e8f1cbb559d38091cefafe5ab9fa3b
MaxSvargal <maxsvargal@gmail.com>
2025-06-18 18:50:32 +0700

    Add engine

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['reactflow', '@reactflow/core', '@reactflow/controls', '@reactflow/background', '@reactflow/minimap'],
-  output: 'export',
-  distDir: 'docs'
+  output: 'export'
 }
 
 module.exports = nextConfig 
